ac3f27aa4e9ce261462f0cff654967a044a4bae9
OnkelTag <jrmcheron@gmail.com>
2025-04-30 12:44:37 +0200

    + | add user list route to gateway

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,20 @@
 const axios = require('axios');
 
 module.exports = async function (fastify, opts) {
+
+    // Users list
+    fastify.get('/users', async (req, reply) => {
+        try {
+            const response = await axios.get('http://user-service:3000/users');
+            return reply.send(response.data);
+        } catch (err) {
+            fastify.log.error(err);
+            const status = err.response?.status || 500;
+            const message = err.response?.data || { error: 'User service error' };
+            return reply.code(status).send(message);
+        }
+    })
+
     // Active user profile
     fastify.get('/users/profile', { preValidation: [fastify.authenticate] }, async (req, reply) => {
         const identifier = req.user.username || req.user.id;
